3d94302915e136a609e76e60db42b82762cc94c8
Carter Moorse <40695530+Carter-Moorse@users.noreply.github.com>
2025-08-22 16:20:32 +0100

    Coverage for functionality

--- src/NodeObserver.tsx
+++ src/NodeObserver.tsx
@@ -1,89 +1,18 @@
-import { ReactElement, createElement, useEffect, useRef, useState } from "react";
+import { ReactElement, createElement, useRef, useState } from "react";
 import { NodeObserverContainerProps } from "../typings/NodeObserverProps";
 import "./ui/NodeObserver.css";
-
-const TEMPNODECLASS = "td-temporary";
-const HIDDENROWCLASS = "tr-hidden";
+import { Observer } from "./components/Observer";
 
 export function NodeObserver({ content, class: className }: NodeObserverContainerProps): ReactElement {
-    const [selected, setSelected] = useState(false);
-    const thisRef = useRef<HTMLDivElement>(null);
+    const [open, setOpen] = useState(false);
 
-    const getDataPosition = (node: HTMLElement) => node.getAttribute("data-position")?.split(",").map(v => Number(v)) || []
+    const divRef = useRef<HTMLDivElement>(null);
 
-    const removeTempNodes = (searchNode?: HTMLElement) => {
-        if (!searchNode) return;
+    Observer({
+        divRef,
+        onUpdate: state => setOpen(state),
+        observeClassName: "tr-selected"
+    })
 
-        searchNode.querySelectorAll(`.${TEMPNODECLASS}`).forEach(node => node.remove());
-    }
-
-    const insertTempNode = (beforeNode: HTMLElement) => {
-        const newNode = document.createElement("div");
-        newNode.classList.add("td", HIDDENROWCLASS, TEMPNODECLASS);
-        newNode.setAttribute("tabindex", "-1");
-
-        beforeNode.parentElement?.insertBefore(newNode, beforeNode);
-        return newNode;
-    }
-
-    useEffect(() => {
-        if (!thisRef) return;
-
-        const dataGridCell = thisRef.current?.parentElement?.parentElement
-        const dataGridRow = dataGridCell?.parentElement;
-        const dataGridBody = dataGridRow?.parentElement;
-        if (!dataGridCell || !dataGridRow || !dataGridBody) return;
-
-        const [columnIndex] = getDataPosition(dataGridCell);
-        if (columnIndex == undefined) return;
-
-        const dataGridHeaderColumns = dataGridBody.querySelector(".tr")?.childNodes as NodeListOf<HTMLElement> | undefined;
-        const dataGridHeaderColumn = (dataGridHeaderColumns || [])[columnIndex] as HTMLElement | undefined;
-        if (!dataGridHeaderColumn) return;
-        
-        // Hide header
-        dataGridHeaderColumn.classList.add(HIDDENROWCLASS);
-        // Hide cell
-        dataGridCell.classList.add(HIDDENROWCLASS);
-        
-        // Callback function to execute when mutations are observed
-        const callback: MutationCallback = (mutationList) => {
-            let isSelected = selected;
-            
-            for (const mutation of mutationList) {
-                if (mutation.type === "attributes" && mutation.attributeName?.includes("class")) {
-                    isSelected = (mutation.target as HTMLElement).classList.contains("tr-selected");
-                    setSelected(isSelected);
-                    break;
-                }
-            }
-
-            const columnCount = dataGridHeaderColumns?.length || 0;
-            const [, rowIndex] = getDataPosition(dataGridCell);
-            
-            if (isSelected) {
-                dataGridCell.classList.remove(HIDDENROWCLASS);
-                dataGridCell.style.gridColumnStart = `span ${columnCount}`;
-                dataGridCell.style.gridRow = `${rowIndex + 3}`
-                dataGridCell.setAttribute("role", "row");
-                removeTempNodes(dataGridRow);
-                insertTempNode(dataGridCell);
-            }
-            else {
-                dataGridCell.classList.add(HIDDENROWCLASS);
-                dataGridCell.style.gridColumnStart = "";
-                dataGridCell.style.gridRow = "";
-                dataGridCell.setAttribute("role", "gridcell");
-                removeTempNodes(dataGridRow);
-            } 
-        };
-        // Create an observer instance linked to the callback function
-        const observer = new MutationObserver(callback);
-        // Start observing the target node for configured mutations
-        observer.observe(dataGridRow, { attributes: true, childList: false, subtree: false });
-        // Disconnect observer on teardown
-        return () => { observer.disconnect(); };
-    }, [thisRef]);
-
-    return <div className={className} ref={thisRef}>{ selected && content }</div>;
+    return <div className={className} ref={divRef}>{open && content}</div>;
 }
---
+++ src/components/Observer.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useRef, RefObject } from "react";
+
+interface ObserverProps {
+    divRef: RefObject<HTMLDivElement>,
+    onUpdate: (state: boolean) => void,
+    observeClassName: string
+}
+
+export function Observer({ divRef, onUpdate, observeClassName }: ObserverProps) {
+    const dataGridCellRef = useRef<HTMLElement | null>();
+    const dataGridRowRef = useRef<HTMLElement | null>();
+    const dataGridBodyRef = useRef<HTMLElement | null>();
+    const dataGridHeaderRef = useRef<HTMLElement | null>();
+    const dataGridHeaderColumnRef = useRef<HTMLElement | null>();
+    const dataGridHeaderColSelectorRef = useRef<HTMLElement | null>();
+
+    const getDataPosition = (node?: HTMLElement | null) => (node && node.getAttribute("data-position")?.split(",").map(v => Number(v))) || []
+    
+    const resetAll = () => {
+        dataGridRowRef.current?.childNodes.forEach(col => resetGrid(col));
+        dataGridHeaderRef.current?.childNodes.forEach(col => resetGrid(col));
+        if (dataGridHeaderColSelectorRef.current) dataGridHeaderColSelectorRef.current.style.display = "";
+    }
+
+    const resetGrid = (node?: HTMLElement | ChildNode | null, all: boolean = false) => {
+        if (!node) return;
+
+        (node as HTMLElement).style.gridColumnStart = "";
+        all && resetGrid(node.previousSibling as HTMLElement | undefined, true);
+    }
+
+    const getState = () => !!dataGridRowRef.current?.classList.contains(observeClassName);
+
+    const toggle = (isOpen: boolean) => {
+        // Update column index, this could change from show/hide columns
+        const [columnIndex] = getDataPosition(dataGridCellRef.current);
+        if (columnIndex == undefined) throw Error("Could not retrieve column index from 'data-position' attribute");
+
+        dataGridHeaderColumnRef.current = (dataGridHeaderRef.current?.childNodes || [])[columnIndex] as HTMLElement | undefined;
+        if (!dataGridHeaderColumnRef.current) throw Error("Could not find column header");
+
+        // Remove header
+        const dataGridHeaderColumnSibling = dataGridHeaderColumnRef.current!.previousSibling as HTMLElement | undefined;
+        dataGridHeaderColumnRef.current!.style.display = "none";
+        if (dataGridHeaderColumnSibling) dataGridHeaderColumnSibling.style.gridColumnStart = "span 2";
+        resetGrid(dataGridHeaderColumnSibling?.previousSibling, true);
+
+        
+        const dataGridSibling = dataGridCellRef.current?.previousSibling as HTMLElement | undefined;
+        const columnCount = dataGridHeaderRef.current?.childNodes.length || 0;
+
+        if (isOpen) {
+            // Open
+            dataGridCellRef.current!.style.display = "";
+            dataGridCellRef.current!.style.gridColumnStart = `span ${columnCount}`;
+            dataGridCellRef.current!.setAttribute("role", "row");
+            if (dataGridHeaderColSelectorRef.current) {
+                dataGridHeaderColSelectorRef.current.style.display = "none";
+                if (dataGridSibling) dataGridSibling!.style.gridColumnStart = "span 3";
+            }
+            else {
+                if (dataGridSibling) dataGridSibling!.style.gridColumnStart = "span 2";
+            }
+        }
+        else {
+            // Close
+            dataGridCellRef.current!.style.display = "none";
+            dataGridCellRef.current!.style.gridColumnStart = "";
+            dataGridCellRef.current!.setAttribute("role", "gridcell");
+            if (dataGridSibling) dataGridSibling!.style.gridColumnStart = "span 2";
+            if (dataGridHeaderColSelectorRef.current) {
+                dataGridHeaderColSelectorRef.current.style.display = "";
+            }
+        }
+
+        resetGrid(dataGridSibling?.previousSibling, true);
+    }
+
+    useEffect(() => {
+        if (!divRef.current) return;
+
+        dataGridCellRef.current = divRef.current.parentElement?.parentElement;
+        dataGridRowRef.current = dataGridCellRef.current?.parentElement;
+        dataGridBodyRef.current = dataGridRowRef.current?.parentElement;
+        dataGridHeaderRef.current = dataGridBodyRef.current?.firstChild as HTMLElement | undefined;
+        if (!dataGridCellRef.current || !dataGridRowRef.current || !dataGridBodyRef.current || !dataGridHeaderRef.current) return;
+        
+        dataGridHeaderColSelectorRef.current = dataGridRowRef.current?.querySelector("& > .column-selector") as HTMLElement | undefined;
+
+        // Inital state
+        const state = getState();
+        toggle(state);
+        onUpdate(state);
+
+        const callback: MutationCallback = () => {
+            const state = getState();
+            toggle(state);
+            onUpdate(state);
+        };
+
+        const observer = new MutationObserver(callback);
+        observer.observe(dataGridRowRef.current, { attributes: true, childList: true, subtree: false });
+
+        return () => {
+            observer.disconnect();
+            resetAll();
+        };
+    }, [divRef]);
+}
